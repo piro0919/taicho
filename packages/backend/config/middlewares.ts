430c3b28d9fe2d5b0da4060363ecfe4e27939418
piro <piro.haniwa@gmail.com>
2022-08-14 00:37:58 +0900

    feat: add enable origin

--- packages/backend/config/middlewares.ts
+++ packages/backend/config/middlewares.ts
@@ -1,12 +1,23 @@
 export default [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      headers: "*",
+      origin: [
+        "http://localhost:1337",
+        "http://localhost:3000",
+        "https://taicho.kk-web.link",
+      ],
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
